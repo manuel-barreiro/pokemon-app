535878b31f1c689caba3976c961e69c2f85dde2f
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-04-29 11:56:51 -0300

    Add new SVG icons and update PokeGrid component

--- src/components/PokeGrid.tsx
+++ src/components/PokeGrid.tsx
@@ -22,7 +22,9 @@ import {
   Image,
   Text,
   Heading,
-  HStack
+  HStack,
+  Divider,
+  Progress
 } from "@chakra-ui/react";
 import TypeBadge from "./PokeCard/TypeBadge";
 
@@ -68,34 +70,63 @@ function PokeGrid({ displayedPokemons }: { displayedPokemons: PokemonData[] }): 
           </Flex>
         </Container>
 
-        <Modal {...pokemonDataModal} size={{ base: 'full', lg:'lg' }}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader textTransform="capitalize" bg={`${selectedPokemon?.types[0].type.name}.badge`} p={5}>
+        <Modal {...pokemonDataModal} size={{ base: 'full', lg:'lg' }} motionPreset='slideInBottom' >
+        <ModalOverlay bg='blackAlpha.300' backdropFilter='blur(10px) hue-rotate(90deg)' />
+        <ModalContent borderTopRadius={100}>
           
+          <ModalHeader textTransform="capitalize" bg={`${selectedPokemon?.types[0].type.name}.badge`} p={5}>
+            <Image onClick={() => pokemonDataModal.onClose()} src={"/chevron-left.svg"} cursor="pointer" width={8} />
           </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody w="full" margin={0} padding={0}>
-            <Flex w="full" direction="column" gap={10} alignItems="start" marginTop={-5}>
-              <Box w="full" h="auto" p={3} display="flex" justifyContent="center" alignItems="center" bg={`${selectedPokemon?.types[0].type.name}.badge`} borderBottomRadius={200} maxH={250} minH={150}>
-                  <Image src={selectedPokemon?.img} maxW={300} marginTop={20} />
+          
+          <ModalBody w="full" margin={0} padding={0} >
+            <Flex w="full" direction="column" gap={10} alignItems="start" marginTop={-1} paddingBottom={10}>
+              <Box w="full" h="auto" p={3} display="flex" justifyContent="center" alignItems="center" bg={`${selectedPokemon?.types[0].type.name}.badge`} borderBottomRadius={200} maxH={{ base: 250, lg: 150 }} minH={{ base: 150, lg: 50 }}>
+                  <Image src={selectedPokemon?.img} maxW={300} marginTop={{ base: 16, lg: 0 }} />
               </Box>
 
-              <Box padding={10} display="flex" flexDirection="column" alignItems="start">
-                <Text fontWeight="bold" fontSize="l">
-                Nº{selectedPokemon?.id.toString().padStart(4, '0')}
-                </Text>
-                <Heading noOfLines={1} fontSize={'3xl'} fontWeight='bold' textAlign="center" textTransform="capitalize" marginBottom={3}>
-                  {selectedPokemon?.name}
-                </Heading>
-                <HStack>
-                  {selectedPokemon?.types.map((type) => (
-                    <TypeBadge key={type.slot} typeName={type.type.name} />
-                  ))}
-                </HStack>          
-              </Box>
+              <Flex w="full" paddingX={10} justifyContent="space-between" alignItems="center">
+                <Box display="flex" flexDirection="column" alignItems="start">
+                  <Text fontWeight="bold" fontSize="sm">
+                  Nº{selectedPokemon?.id.toString().padStart(4, '0')}
+                  </Text>
+                  <Heading noOfLines={1} fontSize={'3xl'} py={1} fontWeight='black' textAlign="center" textTransform="capitalize" marginBottom={3}>
+                    {selectedPokemon?.name}
+                  </Heading>
+                  <HStack>
+                    {selectedPokemon?.types.map((type) => (
+                      <TypeBadge key={type.slot} typeName={type.type.name} />
+                    ))}
+                  </HStack>          
+                </Box> 
 
-            </Flex>
+                <Flex direction="column" gap={2}>
+                  <Box display="flex" flexDirection="row" gap={2}>
+                    <Image src={"/weight.svg"} width={5} />
+                    <Text fontSize="2xl">{(Number(selectedPokemon?.weight) * 0.1).toFixed(1)} kg</Text>
+                  </Box>
+                  <Divider />
+                  <Box display="flex" flexDirection="row" gap={2}>
+                    <Image src={"/ruler.svg"} width={5} />
+                    <Text fontSize="2xl">{(Number(selectedPokemon?.height) * 0.1).toFixed(1)} m</Text>
+                  </Box>
+                </Flex>
+              </Flex>   
+
+
+              <Flex direction="column" gap={1} paddingX={10} w="full">
+                {selectedPokemon?.stats.map((stat, index) =>
+                  <Flex direction="row" alignItems="center" gap={1} >
+                    <Text fontSize="xs" fontWeight="bold" textTransform="uppercase" flexBasis="70%">{stat.stat.name}</Text>
+                    <Flex direction="row" alignItems="center" w="full" gap={1}>
+                      <Text fontSize="xs" textTransform="uppercase">{stat.base_stat.toString().padStart(3, '0')}</Text>
+                      <Progress borderRadius={5} w="full" size="sm" value={stat.base_stat} hasStripe/>
+                    </Flex>
+                  </Flex>
+                )}
+              </Flex>
+            </Flex> 
+
+            
           </ModalBody>
         </ModalContent>
       </Modal>
